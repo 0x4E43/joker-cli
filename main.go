f0b5db8444a37e692f1c1d087c4f5a4c9a75e413
0x4E43 <nimaic.dev@gmail.com>
2024-02-23 22:45:30 +0530

    Basic TLV format

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 )
+
+//ENCODER STARTS
+//  CMD    NO_OF_PARAM   DETAILS
+//  PUT    2             KEY VALUE
+//  GET    1             KEY
+//  DEL    1             KEY
+//ENCODER ENDS
+
+//  PARSER START
+//
+//  PARSER END
+
+//  ERROR CODES START --Will be coming as response
+//    ERR_001 INVALID_CMD
+//    ERR_OO2 KEY_NOT_FOUND
+//    ERR_999 ERR_UNKNOWN
+//  ERROR CODES END
 
 func main() {
 	fmt.Println("Hello World!")
@@ -15,25 +33,29 @@ func main() {
 		panic(err)
 	}
 
-	//ENCODER STARTS
-	// CMD    NO_OF_PARAM   DETAILS
-	// PUT    2             KEY VALUE
-	// GET    1             KEY
-	// DEL    1             KEY
-	//ENCODER ENDS
+	tlv := TLV{
+		Tag:    uint16(1),
+		Length: uint16(len("Hello")),
+		Value:  []byte("Hello"),
+	}
 
-	//  PARSER START
-	//
-	//  PARSER END
-
-	//  ERROR CODES START --Will be coming as response
-	//  ERR_001 INVALID_CMD
-	//  ERR_OO2 KEY_NOT_FOUND
-	//  ERR_999 ERR_UNKNOWN
-	//  ERROR CODES END
-	data := []byte("PUT\r\nKEY\r\nVALUE\r\n")
-	_, err = conn.Write(data)
+	_, err = conn.Write(tlv.Encode())
 	if err != nil {
 		panic(nil)
 	}
+}
+
+type TLV struct {
+	Tag    uint16 //2bit
+	Length uint16 //bit
+	Value  []byte //as per length
+}
+
+func (t *TLV) Encode() []byte {
+	buf := make([]byte, 4+len(t.Value))
+	binary.BigEndian.PutUint16(buf, t.Tag)
+	binary.BigEndian.PutUint16(buf[2:], t.Length)
+	copy(buf[4:], t.Value)
+	fmt.Println(buf)
+	return buf
 }
